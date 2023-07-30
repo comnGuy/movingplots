32402f88f8c460ff78a0c0dd6a0d8a7a11ddc4ff
Bernhard Preisler <bpblub@gmail.com>
2023-07-30 09:09:52 +0200

    chore: cleaned up a lot of code

--- example.py
+++ example.py
@@ -3,8 +3,13 @@ from classes.bar_race_chart import create_bar_race
 
 # load data
 data = pd.read_csv('datasets/edo_cum.csv')
-data = data[:30]
 
+
+# TODO, muss vorher gemacht werden
+# data = data.reset_index()
+data['date'] = pd.to_datetime(data.date, format='%Y-%m-%d')
+data = data[:5]
+print(data)
 # Colors
 colors = ["#adb0ff", "#ffb3ff", "#90d595",
           "#e48381", "#aafbff", "#f7bb5f", "#eafb50"]
@@ -12,7 +17,7 @@ colors = ["#adb0ff", "#ffb3ff", "#90d595",
 create_bar_race(
     data,
     colors,
-    fps=15,
+    fps=2,
     save_path='anim.gif',
     title='Most played gamed by Edopeh',
     bar_chart_text='Accumulated average monthly views',
